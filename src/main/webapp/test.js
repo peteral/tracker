5fc72bbf8ab6f5e0cf2d8ec2046571cb3b866109
peteral <ladislav.petera@yahoo.de>
2015-11-12 20:56:15 +0100

    set content

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -13,13 +13,14 @@ $("#list").on("click", function(event) {
 	$.get("/rest/times/list")
 	.done( function( data ) {
 		var result = $( "#result" );
-		result.html( "<table><tr><th>Client<th>Time" );
+		var tableData = "<table><tr><th>Client<th>Time";
 		
 		for (i in data.entries) {
 			var entry = data.entries[i];
-			result.html( result.html() + "<tr><td>" + entry.clientId + "<td>" + new Time(entry.time.$numberLong) );
+			tableData += "<tr><td>" + entry.clientId + "<td>" + new Time(entry.time.$numberLong);
 		}
 			
-		result.html( result.html() + "</table>" );
+		tableData += "</table>";
+		result.html( tableData );
 	});
 });
