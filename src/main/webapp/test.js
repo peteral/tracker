07026d5171fb9162befe93d09f0c32421b6f28b6
peteral <ladislav.petera@yahoo.de>
2015-11-12 20:17:30 +0100

    handle all ajax events

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -4,14 +4,14 @@
 
 $("#send").on("click", function(event) {
 	$.get("/rest/times/report/" + $("#input").val())
-	.done(function( data ) {
+	.always(function( data ) {
 	    $( "#result" ).html( data );
 	});
 });
 
 $("#list").on("click", function(event) {
 	$.get("/rest/times/list")
-	.done( function( data ) {
+	.always( function( data ) {
 	    $( "#result" ).html( data );
 	});
 });
