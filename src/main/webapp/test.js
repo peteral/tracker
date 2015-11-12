2ea15b826facd70cc3b892bf64cfd9edede8395d
peteral <ladislav.petera@yahoo.de>
2015-11-12 23:49:42 +0100

    improve list output

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -12,6 +12,30 @@ $("#send").on("click", function(event) {
 $("#list").on("click", function(event) {
 	$.get("/rest/times/list")
 	.done( function( data ) {
-		$( "#result" ).html( JSON.stringify( data ));
+		var result = "<ul>";
+		
+		for (i in data.entries) {
+			var client = data.entries[i];
+			
+			result += "<li>" + client.clientId + "<ul>";
+			
+			for (j in client.days) {
+				var day = client.days[j];
+				result += "<li>" + day.day + "<ul>";
+				
+				for (k in day.sessions) {
+					var session = day.sessions[k];
+					result += "<li>" + session.duration.$numberLong + " min (" + 
+					                   new Date(parseInt(session.start.$numberLong)) + " - " + 
+					                   new Date(parseInt(session.end.$numberLong)) + ")";
+				}
+				result += "</ul></li>";
+			}
+			
+			result += "</ul></li>";
+		}
+		
+		result += "</ul>";
+		$( "#result" ).html( result );
 	});
 });
